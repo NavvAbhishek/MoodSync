36928da557fc7b6bba8ab7128fea2a619d587087
Navindu Abhishek <abi2000navi@gmail.com>
2024-10-02 23:51:32 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,18 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p className="bg-red-100 h-20 flex items-center justify-center font-lilitaOne">
-        Design and develop by{" "}
+      <p className="h-20 text-xl flex items-center justify-center font-lilitaOne">
+        Design and develop by
+        <span>&nbsp;</span>
         <Link
           className="text-purple"
           href="https://www.linkedin.com/in/navabhishek/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          {" "}
           Navindu Abhishek
-        </Link>{" "}
+        </Link>
+        <span>&nbsp;</span>
         with 💙
       </p>
     </footer>
