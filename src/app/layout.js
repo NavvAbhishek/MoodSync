977d4663f495a261c10c519d667d5217ea53e9f5
NavvAbhishek <abi2000navi@gmail.com>
2024-11-06 01:07:37 +0530

    add head

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { AuthProvider } from "../context/AuthContext";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Head from "./head";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,10 +25,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head />
       <AuthProvider>
-        <body className={`${lilitaOne.variable} ${poppins.variable} w-full max-w-[1000px] mx-auto min-h-screen flex flex-col antialiased`}>
+        <body
+          className={`${lilitaOne.variable} ${poppins.variable} w-full max-w-[1000px] mx-auto min-h-screen flex flex-col antialiased`}
+        >
           <Navbar />
-          {children}
+          <div className="flex-grow">{children}</div>
           <Footer />
         </body>
       </AuthProvider>
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex flex-col flex-1 justify-center items-center gap-4">
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
       <i className="fa-solid fa-spinner animate-spin text-4xl sm:text-5xl"></i>
     </div>
   );
