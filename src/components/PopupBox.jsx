eec7b842423286cbe2b90f0f501e381233ef16e5
NavvAbhishek <abi2000navi@gmail.com>
2024-12-06 13:22:05 +0530

    add btn to popup box

--- src/components/PopupBox.jsx
+++ src/components/PopupBox.jsx
@@ -1,4 +1,13 @@
-const PopupBox = () => {
+import { useState } from "react";
+
+const PopupBox = ({ onSubmit }) => {
+  const [note, setNote] = useState("");
+
+  const handleSubmit = () => {
+    onSubmit(note);
+    setNote(''); // Optionally clear the textarea after submit
+  };
+
   return (
     <div>
       <h2 className="text-base font-semibold pb-1">Add note</h2>
@@ -6,9 +15,13 @@ const PopupBox = () => {
         className="border-2 border-black rounded-lg p-1"
         rows={3}
         cols={30}
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
       />
       <div>
-        <button className="purpleBtn !px-[10px] !py-2">Add</button>
+        <button className="purpleBtn !px-[10px] !py-2"  onClick={handleSubmit}>
+          Add
+        </button>
       </div>
     </div>
   );
